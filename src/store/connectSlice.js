ba1245a45eb049019cb56d5240e079992ce0851a
Fafemi Adeola <adeola5678@gmail.com>
2022-07-01 23:49:58 +0100

    updates

--- src/store/connectSlice.js
+++ src/store/connectSlice.js
@@ -21,7 +21,6 @@ export const connectSlice = createSlice({
   },
 });
 
-// Action creators are generated for each case reducer function
 export const { connect, disConnect } = connectSlice.actions;
 
 export default connectSlice.reducer;
